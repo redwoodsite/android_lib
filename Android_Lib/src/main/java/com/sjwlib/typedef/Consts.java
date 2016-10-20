e638d65cba9905180b454a97044ff1f24968db41
yzxyangzhixi <yzxyangzhixi@126.com>
2016-10-20 13:57:58 +0800

    1 appManager加入getPrefixKey防止，换用户登陆导致图片张冠李戴。 2 司机货主主要常量移入android_lib和project_lib 注：此版本裁剪后有问题

---
+++ Android_Lib/src/main/java/com/sjwlib/typedef/Consts.java
@@ -0,0 +1,25 @@
+package com.sjwlib.typedef;
+
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+public class Consts {
+
+    /*请求码定义----------------------------------------------------------------------------------*/
+    public static final int REQUEST_CODE_101 = 101;
+    public static final int REQUEST_CODE_102 = 102;
+    public static final int REQUEST_CODE_103 = 103;
+    public static final int REQUEST_CODE_104 = 104;
+    public static final int REQUEST_CODE_105 = 105;
+    public static final int REQUEST_CODE_106 = 106;
+    public static final int REQUEST_CODE_107 = 107;
+    public static final int REQUEST_CODE_108 = 108;
+    public static final int REQUEST_CODE_200 = 200;
+    public static final int REQUEST_CODE_300 = 300;
+    public static final int REQUEST_CODE_400 = 400;
+    public static final int REQUEST_CODE_500 = 500;
+    public static final int OPERATE_CODE_200 = 300;
+    public static final int OPERATE_CODE_300 = 300;
+    public static final int OPERATE_CODE_400 = 400;
+    public static final int OPERATE_CODE_500 = 500;
+}
